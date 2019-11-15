28ae70134268ed536616667dc1e350308603735f
coltynw <coltynw@users.github.com>
2019-11-15 13:24:05 -0500

    coltyn

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,56 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import styled from "styled-components";
+
+
+export default function CharacterCard(props) {
+
+
+// heres the styling for the cards
+
+
+  const Card = styled.div`
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+    margin-top: 1%;
+    margin-bottom: 1%;
+    display: flex;
+    justify-content: space-between;
+  `;
+
+  const CardImg = styled.img`
+    width: auto;
+    height: auto;
+    padding: 0;
+    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  `;
+
+  const CardInfo = styled.div`
+    tex-align: center;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    width: 50%;
+  `;
+
+
+  // the actual card and data props
+
+return (
+  <div className="character-card">
+    <Card>
+      <CardImg src={props.character.image} />
+      <CardInfo>
+        <div>Name: {props.character.name}</div>
+        <div>Episodes: {props.character.episode.length}</div>
+        <div>Gender: {props.character.gender}</div>
+        <div>ID: {props.character.id}</div>
+        <div>Location: {props.character.location.name}</div>
+        <div>Origin: {props.character.origin.name}</div>
+        <div>Species: {props.character.species}</div>
+        <div>Status: {props.character.status}</div>
+      </CardInfo>
+    </Card>
+  </div>
+)
+
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function WelcomePage() {
   return (
-    <section className="welcome-page">
+    <section className='welcome-page'>
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
+        {/* made regular text link to the next page and then made the img link there as well*/}
+        <Link className='listLink' to={'/characters'}>Character List and Search
+        <Link className='listLink' to={'/characters'}></Link><img
+          className='main-img'
+          src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+          alt='rick'
+        /></Link>
       </header>
     </section>
   );
